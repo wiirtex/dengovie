9c60faa000943a8df8b8eaab39f27088ed148734
tyakhshigulov <tyakhshigulov@ozon.ru>
2025-06-12 15:51:22 +0300

    add http://backend.ingress to allowed origins

--- internal/app/middlewares/cors.go
+++ internal/app/middlewares/cors.go
@@ -1,9 +1,18 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var allowList = map[string]bool{
+	"http://localhost:5173":  true,
+	"http://backend.ingress": true,
+}
 
 func CORSMiddleware(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	if origin := c.Request.Header.Get("Origin"); allowList[origin] {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+	}
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")

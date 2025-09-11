ecbba9186abe4e355ed23b0a0257455209643593
GalaxyShad <vadim.kadyrin@mail.ru>
2025-09-11 23:57:20 +0300

    [frontend] istanbul vitest generator

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -1,5 +1,5 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
@@ -18,6 +18,19 @@ export default defineConfig({
       reporter: [
         'text',
         ["lcov", { outputFile: "lcov.info", silent: false }],
+      ],
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/coverage/**',
+        '**/tests/**',
+        '**/*.test.{js,ts,jsx,tsx}',
+        '**/*.spec.{js,ts,jsx,tsx}',
+        '**/__tests__/**',
+        '**/__mocks__/**',
+        '**/types/**',
+        '**/*.{js,ts}', // игнорировать файлы index
+        'src/App.vue',                                                                                                                                                                                                                                        'src/components/DebtsIncomeList.vue', 'src/components/DebtsOutcomeList.vue', 'src/components/MainPage.vue', 'src/components/UserProfilePanel.vue', 'src/components/CashSplitter.vue',
       ]
     }
   },
